eea352038a47aa20506de3b8241cd9988db15ae9
Efren Perez <Efren.perez@wizeline.com>
2018-08-23 12:31:21 -0500

    Initial Commit

---
+++ AniListAlaNetflix/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AniListAlaNetflix
+//
+//  Created by Efrén Pérez Bernabe on 8/23/18.
+//  Copyright © 2018 Efrén Pérez Bernabe. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
